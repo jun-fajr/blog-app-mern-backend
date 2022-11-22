d72c45eee752b8f80fcacf7f05f5761c89f5457b
Jun al-Fajr <jun.fajr@gmail.com>
2022-11-22 11:38:35 +0700

    get all post

--- route/posts/postRoute.js
+++ route/posts/postRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { createPostCtrl } = require('../../controllers/posts/postCtrl')
+const { createPostCtrl, fetchPostsCtrl } = require('../../controllers/posts/postCtrl')
 const authMiddleware = require('../../middlewares/auth/authMiddleware')
 const { photoUpload, postImgResize } = require('../../middlewares/uploads/photoUpload')
 
@@ -7,4 +7,5 @@ const postRoute = express.Router()
 
 postRoute.post('/', authMiddleware, photoUpload.single('image'), postImgResize, createPostCtrl)
 
+postRoute.get('/', fetchPostsCtrl)
 module.exports = postRoute
